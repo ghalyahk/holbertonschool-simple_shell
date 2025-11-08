87203c0976a7877d69e50454d5a0775063f08e34
ghalyahk <ghalyah1166@gmail.com>
2025-11-08 20:39:52 +0000

    Initial simple shell project setup

--- parse_line.c
+++ parse_line.c
@@ -1,28 +1,47 @@
 #include "shell.h"
+#include <stdlib.h>   /* malloc, free, NULL */
+#include <string.h>   /* strtok, strlen, strcmp */
+#include <stddef.h>   /* NULL أيضا إذا أحببت */
+
+#define TOK_BUFSIZE 64
 
 /**
- * parse_line - Split line into arguments
- * @line: input command
+ * parse_line - splits a line into words (tokens)
+ * @line: input string
  *
- * Return: array of strings
+ * Return: array of strings (tokens), last element NULL
  */
 char **parse_line(char *line)
 {
-	char **args = malloc(64 * sizeof(char *));
-	char *token;
-	int i = 0;
+    int bufsize = TOK_BUFSIZE, position = 0;
+    char **args = malloc(bufsize * sizeof(char *));
+    char *token;
 
-	if (!args)
-		return (NULL);
+    if (!args)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
 
-	token = strtok(line, " \n\t");
-	while (token)
-	{
-		args[i] = token;
-		i++;
-		token = strtok(NULL, " \n\t");
-	}
+    token = strtok(line, " \n\t");
+    while (token != NULL)
+    {
+        args[position++] = token;
 
-	args[i] = NULL;
-	return (args);
+        if (position >= bufsize)
+        {
+            bufsize += TOK_BUFSIZE;
+            args = realloc(args, bufsize * sizeof(char *));
+            if (!args)
+            {
+                perror("realloc");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        token = strtok(NULL, " \n\t");
+    }
+    args[position] = NULL;
+    return (args);
 }
+
