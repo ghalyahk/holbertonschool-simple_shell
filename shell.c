63dfe972d34a7acde5f788eee7c090c6a58ee0e7
ghalyahk <ghalyah1166@gmail.com>
2025-11-08 20:31:01 +0000

    Add main.c, shell.c, shell.h

--- shell.c
+++ shell.c
@@ -6,54 +6,68 @@
 
 extern char **environ;
 
-int main(void)
+/**
+ * execute_command - Forks a child process and executes a command
+ * @command: Absolute path of the command to execute
+ *
+ * Description: Executes a single-word command using execve
+ * Return: 0 on success, -1 on failure
+ */
+int execute_command(char *command)
+{
+    pid_t pid;
+    int status;
+    char *argv[2];
+
+    if (command == NULL || command[0] == '\0')
+        return (-1);
+
+    argv[0] = command;
+    argv[1] = NULL;
+
+    pid = fork();
+    if (pid == 0)
+    {
+        execve(argv[0], argv, environ);
+        perror("./shell");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0)
+    {
+        perror("fork");
+        return (-1);
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+    }
+
+    return (0);
+}
+
+/**
+ * read_command - Reads a line from stdin
+ *
+ * Return: pointer to the line (must be freed by caller), or NULL on EOF/error
+ */
+char *read_command(void)
 {
     char *line = NULL;
     size_t len = 0;
     ssize_t nread;
-    pid_t pid;
-    int status;
-    char *argv[2]; /* argv[0] = command, argv[1] = NULL */
 
-    while (1)
+    printf("#cisfun$ ");
+    nread = getline(&line, &len, stdin);
+
+    if (nread == -1)
     {
-        printf("#cisfun$ ");
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            printf("\n");
-            break;
-        }
-
-        if (line[nread - 1] == '\n')
-            line[nread - 1] = '\0';
-
-        if (line[0] == '\0')
-            continue;
-
-        argv[0] = line; /* command */
-        argv[1] = NULL;
-
-        pid = fork();
-        if (pid == 0)
-        {
-            execve(argv[0], argv, environ);
-            perror("./shell"); /* execve failed */
-            exit(EXIT_FAILURE);
-        }
-        else if (pid < 0)
-        {
-            perror("fork");
-            free(line);
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            waitpid(pid, &status, 0);
-        }
+        free(line);
+        return (NULL);
     }
 
-    free(line);
-    return (0);
+    if (line[nread - 1] == '\n')
+        line[nread - 1] = '\0';
+
+    return (line);
 }
 
