10f415da6f1c955657b07f91f35e9b28e252b8fc
ghalyahk <ghalyah1166@gmail.com>
2025-11-08 20:03:46 +0000

    Initial commit for simple_shell project

--- shell.c
+++ shell.c
@@ -1,42 +1,59 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
 
-/**
- * main - Entry point for the simple shell
- *
- * Return: Always 0
- */
+extern char **environ;
+
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	char **args = NULL;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    pid_t pid;
+    int status;
+    char *argv[2]; /* argv[0] = command, argv[1] = NULL */
 
-	while (1)
-	{
-		printf("$ ");
+    while (1)
+    {
+        printf("#cisfun$ ");
+        nread = getline(&line, &len, stdin);
+        if (nread == -1)
+        {
+            printf("\n");
+            break;
+        }
 
-		read = getline(&line, &len, stdin);
-		if (read == -1)
-		{
-			free(line);
-			exit(EXIT_FAILURE);
-		}
+        if (line[nread - 1] == '\n')
+            line[nread - 1] = '\0';
 
-		args = parse_line(line);
-		if (args == NULL)
-			continue;
+        if (line[0] == '\0')
+            continue;
 
-		if (execute_command(args) == -1)
-		{
-			free(args);
-			break;
-		}
+        argv[0] = line; /* command */
+        argv[1] = NULL;
 
-		free(args);
-	}
+        pid = fork();
+        if (pid == 0)
+        {
+            execve(argv[0], argv, environ);
+            perror("./shell"); /* execve failed */
+            exit(EXIT_FAILURE);
+        }
+        else if (pid < 0)
+        {
+            perror("fork");
+            free(line);
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            waitpid(pid, &status, 0);
+        }
+    }
 
-	free(line);
-	return (0);
+    free(line);
+    return (0);
 }
 
