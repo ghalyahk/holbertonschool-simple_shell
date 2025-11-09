c388149b08fbfa58d29a602afde74ef9b35049f6
ghalyahk <ghalyah1166@gmail.com>
2025-11-09 22:50:11 +0000

    holbertonschool-simple_shell

--- shell.c
+++ shell.c
@@ -6,7 +6,6 @@ int main(void)
     size_t len = 0;
     char **args;
     char *cmd_path;
-    int status = 0;
 
     while (1)
     {
@@ -16,7 +15,7 @@ int main(void)
         if (getline(&line, &len, stdin) == -1)
         {
             free(line);
-            exit(status);
+            exit(0);
         }
 
         args = tokenize(line);
@@ -26,30 +25,31 @@ int main(void)
             continue;
         }
 
+        /* Built-in exit */
         if (strcmp(args[0], "exit") == 0)
         {
             free_tokens(args);
             free(line);
-            exit(status);
+            exit(0);
         }
 
         cmd_path = find_path(args[0]);
 
         if (cmd_path == NULL)
         {
+            /* اطبع رسالة خطأ مشابهة bash عند PATH فارغ */
             fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
             free_tokens(args);
-            status = 127; /* <- تعيين الـ exit status عند أمر غير موجود */
             continue;
         }
 
-        status = execute(cmd_path, args);
+        execute(cmd_path, args);
 
         free(cmd_path);
         free_tokens(args);
     }
 
     free(line);
-    return status;
+    return 0;
 }
 
