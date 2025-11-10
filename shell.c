ac69e6766e9b68e563bc09d63eb2571f83608694
ghalyahk <ghalyah1166@gmail.com>
2025-11-10 15:59:02 +0000

    Write your own getline function

--- shell.c
+++ shell.c
@@ -2,60 +2,56 @@
 
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	char **args;
-	char *cmd_path;
-	int last_status = 0;
+    char *line = NULL;
+    size_t len = 0;
+    char **args;
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			prompt();
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            prompt();
 
-		if (getline(&line, &len, stdin) == -1)
-		{
-			free(line);
-			exit(last_status);
-		}
+        if (getline(&line, &len, stdin) == -1)
+        {
+            free(line);
+            exit(0);
+        }
 
-		args = tokenize(line);
-		if (!args || !args[0])
-		{
-			free_tokens(args);
-			continue;
-		}
+        args = tokenize(line);
+        if (!args || !args[0])
+        {
+            free_tokens(args);
+            continue;
+        }
 
-		if (strcmp(args[0], "exit") == 0)
-		{
-			free_tokens(args);
-			free(line);
-			exit(last_status);
-		}
+        if (strcmp(args[0], "exit") == 0)
+        {
+            free_tokens(args);
+            free(line);
+            exit(0);
+        }
 
-		if (strcmp(args[0], "env") == 0)
-		{
-			print_env();
-			free_tokens(args);
-			continue;
-		}
+        if (strcmp(args[0], "env") == 0)
+        {
+            print_env();
+            free_tokens(args);
+            continue;
+        }
 
-		cmd_path = find_path(args[0]);
+        if (find_path(args[0]) == NULL)
+        {
+            fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+            free_tokens(args);
+            free(line);
+            exit(127);
+        }
 
-		if (cmd_path == NULL)
-		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-			free_tokens(args);
-			free(line);
-			exit(127);
-		}
+        execute(args);
 
-		last_status = execute(cmd_path, args);
+        free_tokens(args);
+    }
 
-		free(cmd_path);
-		free_tokens(args);
-	}
-
-	free(line);
-	return (0);
+    free(line);
+    return (0);
 }
+
