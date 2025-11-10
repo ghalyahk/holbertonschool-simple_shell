b362fe07b5b36ab466e2b8cc47e1ff7530371f82
ghalyahk <ghalyah1166@gmail.com>
2025-11-10 15:42:02 +0000

    Write your own getline function

--- shell.h
+++ shell.h
@@ -1,11 +1,22 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/* ===== LIBRARIES YOU MUST INCLUDE ===== */
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
 
+/* لازم نكتب التصريح الخاص بـ environ */
+extern char **environ;
+
+/* prototypes */
 ssize_t _getline(char **lineptr, size_t *n);
+char **tokenize(char *line);
+void execute(char **args);
+void prompt(void);
+char *find_path(char *command);
 
 #endif
 
