1902e9b4176d3c04742e889ed43f1cc67b389e8b
roya86 <royaabduallah@gmail.com>
2025-11-10 11:43:06 +0000

    Initial commit for simple_shell project

--- execute.c
+++ execute.c
@@ -1,19 +1,31 @@
 #include "shell.h"
 
+/**
+ * execute - creates a child process to execute a command
+ * @cmd: command path
+ * @args: command arguments
+ */
 void execute(char *cmd, char **args)
 {
-    pid_t pid = fork();
+	pid_t pid = fork();
+	int status;
 
-    if (pid == 0)
-    {
-        execve(cmd, args, environ);
-        perror(args[0]);
-        exit(127);
-    }
-    else if (pid > 0)
-    {
-        int status;
-	wait(&status);
-    }
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+
+	if (pid == 0)
+	{
+		
+		execve(cmd, args, environ);
+		perror(args[0]);
+		exit(127);
+	}
+	else
+	{
+		
+		wait(&status);
+	}
 }
-
--- shell.c
+++ shell.c
@@ -2,54 +2,54 @@
 
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    char **args;
-    char *cmd_path;
+	char *line = NULL;
+	size_t len = 0;
+	char **args;
+	char *cmd_path;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            prompt();
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			prompt();
 
-        if (getline(&line, &len, stdin) == -1)
-        {
-            free(line);
-            exit(0);
-        }
+		if (getline(&line, &len, stdin) == -1)
+		{
+			free(line);
+			exit(0);
+		}
 
-        args = tokenize(line);
-        if (!args || !args[0])
-        {
-            free_tokens(args);
-            continue;
-        }
+		args = tokenize(line);
+		if (!args || !args[0])
+		{
+			free_tokens(args);
+			continue;
+		}
 
-        /* Built-in exit */
-        if (strcmp(args[0], "exit") == 0)
-        {
-            free_tokens(args);
-            free(line);
-            exit(0);
-        }
+	
+		if (strcmp(args[0], "exit") == 0)
+		{
+			free_tokens(args);
+			free(line);
+			exit(0);
+		}
 
-        cmd_path = find_path(args[0]);
+		cmd_path = find_path(args[0]);
 
-        if (cmd_path == NULL)
-        {
-            
-            fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-            free_tokens(args);
-            exit(127);
-        }
+		if (cmd_path == NULL)
+		{
+			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			free_tokens(args);
+			free(line);
+			exit(127);
+		}
 
-        execute(cmd_path, args);
+		execute(cmd_path, args);
 
-        free(cmd_path);
-        free_tokens(args);
-    }
+	
+		free(cmd_path);
+		free_tokens(args);
+	}
 
-    free(line);
-    return 0;
+	free(line);
+	return (0);
 }
-
