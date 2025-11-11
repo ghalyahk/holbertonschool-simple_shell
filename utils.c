56758534d991efbae2b37936653f0c2599d2a304
roya86 <royaabduallah@gmail.com>
2025-11-11 12:59:59 +0000

    Write your own getline function

--- utils.c
+++ utils.c
@@ -2,6 +2,56 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/* my_getline: قراءة سطر كامل من stdin */
+ssize_t my_getline(char **lineptr, size_t *n)
+{
+    static char buffer[1024];
+    static ssize_t buf_pos = 0, buf_len = 0;
+    ssize_t i = 0;
+    char *buf;
+
+    if (!lineptr)
+        return -1;
+
+    if (!*lineptr)
+    {
+        *n = 1024;
+        *lineptr = malloc(*n);
+        if (!*lineptr)
+            return -1;
+    }
+
+    buf = *lineptr;
+
+    while (1)
+    {
+        if (buf_pos >= buf_len)
+        {
+            buf_len = read(STDIN_FILENO, buffer, 1024);
+            if (buf_len <= 0)
+                return -1;
+            buf_pos = 0;
+        }
+
+        buf[i] = buffer[buf_pos++];
+        if (buf[i] == '\n')
+        {
+            buf[i + 1] = '\0';
+            return i + 1;
+        }
+
+        i++;
+        if ((size_t)i >= *n - 1)
+        {
+            *n *= 2;
+            buf = realloc(buf, *n);
+            if (!buf)
+                return -1;
+            *lineptr = buf;
+        }
+    }
+}
+
 /* my_strdup: نسخ سلسلة */
 char *my_strdup(const char *s)
 {
@@ -38,8 +88,10 @@ int my_strncmp(const char *s1, const char *s2, size_t n)
 {
     int i;
     for (i = 0; i < (int)n && s1[i] && s2[i]; i++)
+    {
         if (s1[i] != s2[i])
             return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+    }
     if (i < (int)n)
         return ((unsigned char)s1[i] - (unsigned char)s2[i]);
     return 0;
@@ -54,8 +106,6 @@ char *my_strchr(const char *s, int c)
             return (char *)s;
         s++;
     }
-    if (c == 0)
-        return (char *)s;
     return NULL;
 }
 
@@ -69,7 +119,5 @@ void free_tokens(char **tokens)
 
     for (i = 0; tokens[i]; i++)
         free(tokens[i]);
-
     free(tokens);
 }
-
